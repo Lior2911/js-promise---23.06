2e37fa17116e00f61393ff5246c866c9f9589dba
lior2911 <aytegev1@gmail.com>
2022-06-26 09:02:10 +0300

    commit2

--- main.js
+++ main.js
@@ -1,47 +1,100 @@
-function getTime(){
+function getTime() {
   const NowDate = new Date();
-  return new Promise((resolve,reject)=>{
-    if(NowDate.getHours()<11){
+  return new Promise((resolve, reject) => {
+    if (NowDate.getHours() < 11) {
       resolve("good morning");
     }
     reject("good afternoon");
-  })
- 
+  });
 }
 
-// function clickME(){
-//   // getTime()
-//   getModulu(num1,num2)
-//   .then((data)=>{(printLine.innerText = data)})
-//   .catch((data)=>{(printLine.innerText = data)})
+function clickME() {
+  getModulu(num1, num2)
+    .then((data) => {
+      printLine.innerText = data;
+    })
+    .catch((data) => {
+      printLine.innerText = data;
+    });
+}
 
-// }
+function getModulu(num1, num2) {
+  return new Promise((resolve, reject) => {
+    if (num2 % num1 == 0) {
+      resolve("yes");
+    }
+    reject("no");
+  });
+}
+function printModulu() {
+  getModulu(2, 3)
+    .then((data) => {
+      printLine.innerText = data;
+    })
+    .catch((err) => {
+      printLine.innerText = err;
+    });
+}
 
-// function getModulu(num1,num2){
-//   return new Promise ((resolve,reject)=>{
-//     if(num2%num1 == 0){
-//       resolve("yes")
+function getPromise(num, arrayNum) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < arrayNum.length; i++) {
+      if (arrayNum[i] == num) {
+        resolve("yes");
+      }
+      reject("no");
+    }
+  });
+}
+function clickTOFindNum() {
+  getPromise(12, [12, 32, 33, 22])
+    .then((data) => (printLine.innerText = data))
+    .catch((err) => (printLine.innerText = err));
+}
 
-//     }
-//     reject("no")
-//   })
-// }
-// getModulu(2,3)
-// .then((data)=>{(printLine.innerText = data)})
-// .catch((data)=>{(printLine.innerText = data)})
+async function callToPromise() {
+  try {
+    await getPromise(num, arrayNum).then((data) => {
+      printLine.innerText = data;
+    });
+  } catch {
+    getPromise(num, arrayNum).catch(() => {
+      printLine.innerText = err;
+    });
+  }
+}
+callToPromise();
 
-// function getPromise(num,arrayNum){
-// return new Promise ((resolve,reject)=>{
-//   for(let i=0 ;i<arrayNum.length;i++){
-//     if(arrayNum[i]==num){
-//       resolve("yes");
-//     }
-//     reject("no")
-//   }
-// })
-// }
-// getPromise(1,[12,32,33,22])
-// .then((data)=>printLine.innerText = data)
-// .catch((data)=>printLine.innerText = data)
+async function getFromFetch() {
+  try {
+    fetchBtn.disabeld = true;
+    await fetch("https://moviesmern.herokuapp.com/movies/all").then(
+      (response) => {
+        console.log(response);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    fetchBtn.disabeld = true;
+  }
+}
+
+async function getFetchFRomHeroku() {
+  try {
+    fetchHeroko.disabeld = true;
+    herokuDiv.innerHTML = "<img style='width:10vw;height: 10vh;' src = 'https://i.stack.imgur.com/ATB3o.gif'"
 
 
+    await fetch("https://cat-fact.herokuapp.com/facts")
+    .then((resp) => {
+
+      console.log(resp);
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    fetchHeroko.disabeld = false;
+    herokuDiv.innerHTML = " "
+  }
+}
